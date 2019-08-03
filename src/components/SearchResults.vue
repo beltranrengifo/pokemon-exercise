74fae3c7b2f51c3e5a4c2e8065646503147ee2f4
beltranrengifo <beltran@rengifo.es>
2019-08-03 13:28:24 +0200

    ADD search improvements in RT

---
+++ src/components/SearchResults.vue
@@ -0,0 +1,66 @@
+<template>
+  <ul class="search-results">
+    <li
+      class="search-results__item"
+      v-for="item in filteredItems"
+      :key="item.name"
+      @click="handleItemClick(item.name)">
+      <span> {{ item.name }} </span>
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  name: 'SearchResults',
+
+  props: {
+    items: {
+      type: Array,
+      required: true
+    },
+
+    val: {
+      type: String,
+      required: true
+    }
+  },
+
+  computed: {
+    filteredItems () {
+      return this.items.filter(e => {
+        return e.name.includes(this.val)
+      })
+    }
+  },
+
+  methods: {
+    handleItemClick (name) {
+      this.$emit('itemSelected', name)
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  .search-results {
+    position: absolute;
+    width: 100%;
+    z-index: 9;
+    background: white;
+    box-shadow: 0px 4px 6px -1px rgba(0,0,0,0.15);
+    max-height: 380px;
+    overflow-y: scroll;
+
+    &__item {
+      line-height: 32px;
+      text-transform: capitalize;
+      border-bottom: 1px dotted $--color-grey;
+      cursor: pointer;
+      transition: background .15s;
+      &:hover {
+        background: lighten($--color-grey, 8%);
+      }
+    }
+  }
+</style>
--- src/views/Search.vue
+++ src/views/Search.vue
@@ -1,36 +1,23 @@
 <template>
   <section class="search">
-    <div
-      class="error"
-      v-if="isError">
-      <p
-        class="error-text"
-        v-html="errorText">
-      </p>
-      <div>
-        <button
-          @click='resetSearch'>Try again</button>
+    <div class="search__input">
+      <input
+        type="text"
+        :placeholder="placeholderText"
+        v-model="inputValue"
+        @keyup.enter="getPokemon"
+        ref="inputSearchPokemon">
+        <search-results
+          :items="pokemons"
+          v-show="inputValue"
+          :val="inputValue"
+          @itemSelected="getPokemon"/>
+      <div class="search__button">
       </div>
     </div>
-    <div v-else>
-      <div class="search__input">
-        <input
-          type="text"
-          :placeholder="placeholderText"
-          v-model="inputValue"
-          @input="checkDisabled"
-          @keyup.enter="getPokemon"
-          ref="inputSearchPokemon">
-        <div class="search__button">
-          <button
-            :disabled="isDisabled"
-            @click="getPokemon">{{ buttonText }}</button>
-        </div>
-      </div>
-      <div class="search__result">
-        <pokemon-detail-item
-          :pokemon="pokemon"/>
-      </div>
+    <div class="search__result">
+      <pokemon-detail-item
+        :pokemon="pokemon"/>
     </div>
   </section>
 </template>
@@ -38,81 +25,65 @@
 <script>
 import axios from 'axios'
 import PokemonDetailItem from '@/components/PokemonDetailItem'
+import SearchResults from '@/components/SearchResults'
 
 export default {
   name: 'Search',
 
   components: {
-    PokemonDetailItem
+    PokemonDetailItem,
+    SearchResults
   },
 
   data () {
     return {
+      pokemons: [],
       pokemon: null,
       inputValue: '',
-      isDisabled: true,
-      isError: false,
-      placeholderText: 'Pokemon name',
-      buttonText: 'Search'
+      placeholderText: 'Pokemon name'
     }
   },
 
   methods: {
-    async getPokemon () {
+    async getAllPokemons () {
       try {
-        let res = await axios.get(`https://pokeapi.co/api/v2/pokemon/${this.inputValue.toLowerCase()}`)
-        this.pokemon = res.data
-        this.resetSearch()
+        let res = await axios.get(`http://pokeapi.co/api/v2/pokemon/?limit=964`)
+        this.pokemons = res.data.results
       } catch (e) {
-        this.isError = true
+        console.log(e)
       }
     },
 
-    checkDisabled () {
-      this.isDisabled = this.inputValue === '' || this.inputValue.length < 3
+    async getPokemon (name) {
+      try {
+        let res = await axios.get(`https://pokeapi.co/api/v2/pokemon/${name.toLowerCase()}`)
+        this.pokemon = res.data
+        this.resetSearch()
+      } catch (e) {
+        console.log(e)
+      }
     },
 
     resetSearch () {
       this.inputValue = ''
-      this.checkDisabled()
-      this.isError = false
       this.$nextTick(_ => {
         this.$refs.inputSearchPokemon.focus()
       })
     }
   },
 
-  computed: {
-    errorText () {
-      return `Pokemon <strong>${this.inputValue}</strong> does not exist!`
-    }
+  created () {
+    this.getAllPokemons()
   }
 }
 </script>
 
 <style lang="scss" scoped>
   .search {
-    &__input,
-    .error {
+    &__input {
       width: 340px;
       margin: 0 auto;
-    }
-
-    .error {
-      .error-text {
-        height: $--input-height;
-        line-height: $--input-height;
-        font-size: 80%;
-      }
-      button {
-        margin-top: 24px;
-      }
-    }
-
-    &__button {
-      button {
-        margin-top: 24px;
-      }
+      position: relative;
     }
   }
 </style>
